7b8f0c2ee2c712d4ba70d2b00656af1fae1fdb91
vadkutsen <vadkutsen@gmail.com>
2022-06-23 18:38:19 +0200

    Deploy updates

---
+++ client/deploy.sh
@@ -0,0 +1,23 @@
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:vadkutsen/medo.git main:gh-pages
+
+cd -
