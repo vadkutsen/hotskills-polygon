4b486d3b9a231ab0639c7536e6fa5e5dead56bf3
vadkutsen <vadkutsen@gmail.com>
2022-06-28 13:51:25 +0200

    Update tests

--- smart_contract/test/platform-test.js
+++ smart_contract/test/platform-test.js
@@ -29,10 +29,10 @@ describe("Plarform contract", function () {
 
   // You can nest describe calls to create subsections.
   describe("Deployment", function () {
-    // `it` is another Mocha function. This is the one you use to define your
-    // tests. It receives the test name, and a callback function.
 
     it("Should add project", async function () {
+      const oldBalance = await ethers.provider.getBalance(hardhatPlatform.address);
+      const fees = await hardhatPlatform.totalFees();
       const addProject = await hardhatPlatform
         .connect(addr1)
         .addProject({
@@ -46,6 +46,8 @@ describe("Plarform contract", function () {
       await addProject.wait();
       const getAllProjects = await hardhatPlatform.getAllProjects();
       expect(await getAllProjects.length).to.equal(2);
+      expect(await ethers.provider.getBalance(hardhatPlatform.address)).to.equal(parseInt(oldBalance) + 101);
+      expect(await hardhatPlatform.totalFees()).to.equal(parseInt(fees) + 1);
     });
 
     it("Cannot add project with invalid amount", async function () {
@@ -157,6 +159,7 @@ describe("Plarform contract", function () {
     });
 
     it("Should complete project", async function () {
+      const oldPlatformBalance = await ethers.provider.getBalance(hardhatPlatform.address);
       const applyForProject = await hardhatPlatform
         .connect(addr2)
         .applyForProject(0);
@@ -166,6 +169,7 @@ describe("Plarform contract", function () {
       const getProject = await hardhatPlatform.getProject(0);
       expect(await getProject.completedAt).to.be.above(0);
       expect(await hardhatPlatform.getRating(addr2.address)).to.equal(5);
+      expect(await ethers.provider.getBalance(hardhatPlatform.address)).to.equal(parseInt(oldPlatformBalance) - 100);
     });
 
     it("Should delete project", async function () {
@@ -180,18 +184,22 @@ describe("Plarform contract", function () {
           value: 101,
         });
       await addProject.wait();
+      const oldBalance = await ethers.provider.getBalance(hardhatPlatform.address);
       await hardhatPlatform.connect(addr1).deleteProject(1);
       const getAllProjects = await hardhatPlatform.getAllProjects();
       expect(await getAllProjects.length).to.equal(1);
       await expect(hardhatPlatform.getProject(1)).to.be.revertedWith(
         "Project does not exist"
       );
+      expect(await ethers.provider.getBalance(hardhatPlatform.address)).to.equal(parseInt(oldBalance) - 100);
     });
 
     it("Should withdraw", async function () {
+      const oldBalance = await ethers.provider.getBalance(hardhatPlatform.address);
       expect(await hardhatPlatform.totalFees()).to.equal(1);
       await hardhatPlatform.withdrawFees();
       expect(await hardhatPlatform.totalFees()).to.equal(0);
+      expect(await ethers.provider.getBalance(hardhatPlatform.address)).to.equal(parseInt(oldBalance) - 1);
     });
 
     it("Should set the right owner", async function () {
@@ -206,62 +214,5 @@ describe("Plarform contract", function () {
       await hardhatPlatform.setPlatformFee(2);
       expect(await hardhatPlatform.platformFeePercentage()).to.equal(2);
     });
-
-
-
-    //   it("Should assign the total supply of tokens to the owner", async function () {
-    //     const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    //     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-    //   });
-    // });
-
-    // describe("Transactions", function () {
-    //   it("Should transfer tokens between accounts", async function () {
-    //     // Transfer 50 tokens from owner to addr1
-    //     await hardhatToken.transfer(addr1.address, 50);
-    //     const addr1Balance = await hardhatToken.balanceOf(addr1.address);
-    //     expect(addr1Balance).to.equal(50);
-
-    //     // Transfer 50 tokens from addr1 to addr2
-    //     // We use .connect(signer) to send a transaction from another account
-    //     await hardhatToken.connect(addr1).transfer(addr2.address, 50);
-    //     const addr2Balance = await hardhatToken.balanceOf(addr2.address);
-    //     expect(addr2Balance).to.equal(50);
-    //   });
-
-    //   it("Should fail if sender doesn’t have enough tokens", async function () {
-    //     const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
-
-    //     // Try to send 1 token from addr1 (0 tokens) to owner (1000000 tokens).
-    //     // `require` will evaluate false and revert the transaction.
-    //     await expect(
-    //       hardhatToken.connect(addr1).transfer(owner.address, 1)
-    //     ).to.be.revertedWith("Not enough tokens");
-
-    //     // Owner balance shouldn't have changed.
-    //     expect(await hardhatToken.balanceOf(owner.address)).to.equal(
-    //       initialOwnerBalance
-    //     );
-    //   });
-
-    //   it("Should update balances after transfers", async function () {
-    //     const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
-
-    //     // Transfer 100 tokens from owner to addr1.
-    //     await hardhatToken.transfer(addr1.address, 100);
-
-    //     // Transfer another 50 tokens from owner to addr2.
-    //     await hardhatToken.transfer(addr2.address, 50);
-
-    //     // Check balances.
-    //     const finalOwnerBalance = await hardhatToken.balanceOf(owner.address);
-    //     expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(150));
-
-    //     const addr1Balance = await hardhatToken.balanceOf(addr1.address);
-    //     expect(addr1Balance).to.equal(100);
-
-    //     const addr2Balance = await hardhatToken.balanceOf(addr2.address);
-    //     expect(addr2Balance).to.equal(50);
-    //   });
   });
 });
