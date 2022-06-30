3d1c31a114a681349790fce5bf2dd77bf30c71e7
vadkutsen <vadkutsen@gmail.com>
2022-06-30 12:23:36 +0200

    Update contract events

--- smart_contract/contracts/Platform.sol
+++ smart_contract/contracts/Platform.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Platform is Ownable, ReentrancyGuard {
+
     enum ProjectType {
         FCFS,
         AuthorSelected
@@ -45,12 +46,9 @@ contract Platform is Ownable, ReentrancyGuard {
     mapping(address => uint8) ratings;
 
     // Events
-    event ProjectAdded(uint256 _id, address _authorAddress);
-    event CandidateApplied(uint256 _id, address _candidateAddress);
-    event ProjectAssigned(uint256 _id, address _assigneeAddress);
-    event ProjectUnassigned(uint256 _id);
-    event ResultSubmitted(uint256 _id, string _result);
-    event ProjectCompleted(uint256 _id);
+
+    event ProjectAdded(Project _project);
+    event ProjectUpdated(Project _project);
     event ProjectDeleted(uint256 _id);
     event FeeUpdated(uint8 _fee);
 
@@ -115,6 +113,30 @@ contract Platform is Ownable, ReentrancyGuard {
         _;
     }
 
+    // Helper functions
+
+    function isAddressApplied(uint256 _id, address _address)
+        public
+        view
+        returns (bool)
+    {
+        for (uint256 i = 0; i < projects[_id].candidates.length; i++) {
+            if (projects[_id].candidates[i].candidate == _address) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function calculateRating(uint _prevRating, uint8 _newRating) internal pure returns (uint8) {
+        if (_prevRating == 0) {
+            return _newRating;
+        }
+        return uint8(_prevRating + _newRating) / 2;
+    }
+
+    // Contract functions
+
     function addProject(ReceivedProject calldata _newProject)
         external
         payable
@@ -140,21 +162,8 @@ contract Platform is Ownable, ReentrancyGuard {
         projects[_id].reward = _newProject.reward;
         projects[_id].projectType = _newProject.projectType;
         projectsCounter++;
-        emit ProjectAdded(_id, msg.sender);
+        emit ProjectAdded(projects[_id]);
         return true;
-    }
-
-    function isAddressApplied(uint256 _id, address _address)
-        public
-        view
-        returns (bool)
-    {
-        for (uint256 i = 0; i < projects[_id].candidates.length; i++) {
-            if (projects[_id].candidates[i].candidate == _address) {
-                return true;
-            }
-        }
-        return false;
     }
 
     function assignProject(uint256 _id, address payable _candidateAddress)
@@ -174,7 +183,7 @@ contract Platform is Ownable, ReentrancyGuard {
             "Address didn't apply to the project."
         );
         projects[_id].assignee = _candidateAddress;
-        emit ProjectAssigned(_id, _candidateAddress);
+        emit ProjectUpdated(projects[_id]);
         return true;
     }
 
@@ -186,7 +195,7 @@ contract Platform is Ownable, ReentrancyGuard {
         returns (bool)
     {
         delete projects[_id].assignee;
-        emit ProjectUnassigned(_id);
+        emit ProjectUpdated(projects[_id]);
         return true;
     }
 
@@ -201,11 +210,11 @@ contract Platform is Ownable, ReentrancyGuard {
         );
         if (projects[_id].projectType == ProjectType.FCFS) {
             projects[_id].assignee = payable(msg.sender);
-            emit ProjectAssigned(_id, msg.sender);
+            emit ProjectUpdated(projects[_id]);
         } else {
             projects[_id].candidates.push(Candidate(msg.sender, ratings[msg.sender]));
         }
-        emit CandidateApplied(_id, msg.sender);
+        emit ProjectUpdated(projects[_id]);
         return true;
     }
 
@@ -218,7 +227,7 @@ contract Platform is Ownable, ReentrancyGuard {
         require(bytes(_result).length > 0, "Result cannot be empty.");
         require(bytes(projects[_id].result).length == 0, "Result already submitted.");
         projects[_id].result = _result;
-        emit ResultSubmitted(_id, _result);
+        emit ProjectUpdated(projects[_id]);
         return true;
     }
 
@@ -235,11 +244,11 @@ contract Platform is Ownable, ReentrancyGuard {
         require(_rating <= 5, "Rating is invalid.");
         projects[_id].completedAt = block.timestamp;
         ratings[projects[_id].assignee] = calculateRating(ratings[projects[_id].assignee], _rating);
-        emit ProjectCompleted(_id);
         (bool success, ) = projects[_id].assignee.call{
             value: projects[_id].reward
         }("");
         require(success, "Tranfer failed.");
+        emit ProjectUpdated(projects[_id]);
         return true;
     }
 
@@ -255,12 +264,8 @@ contract Platform is Ownable, ReentrancyGuard {
             projects[_id].assignee == address(0),
             "Project already assigned."
         );
-        // uint256 platformFee = (projects[_id].reward / 100) *
-        //     platformFeePercentage;
-        // uint256 amount = projects[_id].reward + platformFee;
         (bool success, ) = projects[_id].author.call{value: projects[_id].reward}("");
         require(success, "Tranfer failed.");
-        // totalFees = totalFees - platformFee;
         projectsCounter = projectsCounter - 1;
         delete projects[_id];
         emit ProjectDeleted(_id);
@@ -281,6 +286,7 @@ contract Platform is Ownable, ReentrancyGuard {
     }
 
     // Getters
+
     function getAllProjects() public view returns (Project[] memory) {
         Project[] memory projectList = new Project[](projectsCounter);
         for (uint256 i = 0; i < projectsCounter; i++) {
@@ -290,23 +296,6 @@ contract Platform is Ownable, ReentrancyGuard {
         }
         return projectList;
     }
-
-    // function get20Projects(uint256 _from)
-    //     public
-    //     view
-    //     returns (uint256[20] memory, uint256)
-    // {
-    //     uint256[20] memory tempProjects;
-    //     uint256 count = 0;
-    //     uint256 i = allProjects.length - 1 - _from;
-    //     for (i; i >= 0 && count < 20; i--) {
-    //         if (allProjects[i] != 0) {
-    //             tempProjects[count] = allProjects[i];
-    //             count++;
-    //         }
-    //     }
-    //     return (tempProjects, allProjects.length - 1 - i);
-    // }
 
     function getProject(uint256 _id)
         public
@@ -336,53 +325,4 @@ contract Platform is Ownable, ReentrancyGuard {
     {
         return ratings[_address];
     }
-
-    function calculateRating(uint _prevRating, uint8 _newRating) internal pure returns (uint8) {
-        if (_prevRating == 0) {
-            return _newRating;
-        }
-        return uint8(_prevRating + _newRating) / 2;
-    }
-
-    // function getProjectsByAssignee(address _assigneeAddress)
-    //     public
-    //     view
-    //     returns (uint256[] memory)
-    // {
-    //     require(_assigneeAddress != address(0), "Zerro address passed");
-    //     uint256[] memory _tempProjects = new uint256[](projectsCounter);
-    //     uint256 counter;
-    //     for (uint256 i = 0; i < projectsCounter; i++) {
-    //         if (projects[i].assignee == _assigneeAddress) {
-    //             _tempProjects[counter] = projects[i];
-    //             counter++;
-    //         }
-    //     }
-    //     uint256[] memory _projects = new uint256[](counter);
-    //     for (uint256 i = 0; i < counter; i++) {
-    //         _projects[i] = _tempProjects[i];
-    //     }
-    //     return _projects;
-    // }
-
-    // function getProjectsByAuthor(address _authorAddress)
-    //     public
-    //     view
-    //     returns (uint256[] memory)
-    // {
-    //     require(_authorAddress != address(0), "Zerro address passed");
-    //     uint256[] memory _tempProjects = new uint256[](allProjects.length);
-    //     uint256 counter;
-    //     for (uint256 i = 0; i < allProjects.length; i++) {
-    //         if (projects[allProjects[i]].author == _authorAddress) {
-    //             _tempProjects[counter] = allProjects[i];
-    //             counter++;
-    //         }
-    //     }
-    //     uint256[] memory _projects = new uint256[](counter);
-    //     for (uint256 i = 0; i < counter; i++) {
-    //         _projects[i] = _tempProjects[i];
-    //     }
-    //     return _projects;
-    // }
 }
