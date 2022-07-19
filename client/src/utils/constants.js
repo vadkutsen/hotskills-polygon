c0236709a400687aa280fd1e00a2f5d540e51ebb
vadkutsen <vadkutsen@gmail.com>
2022-07-19 20:26:36 +0200

    Update contract

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./contractABI.json";
 
-export const contractAddress = "0x197aF0CB5Ca4d1ca8f0198009379527286757C55";
+export const contractAddress = "0x3aD0caD9E7Fb1A7A46F6F0665bbC97BdfDdaD740";
 export const contractABI = abi.abi;
