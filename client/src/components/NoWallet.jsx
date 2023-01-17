89a72da400c73184ce696e9d62813b30c44dd7dc
vadkutsen <vadkutsen@gmail.com>
2023-01-17 18:22:06 +0100

    Update message wording

--- client/src/components/NoWallet.jsx
+++ client/src/components/NoWallet.jsx
@@ -4,7 +4,7 @@ function NoWallet() {
   return (
     <div className="h-screen flex flex-col w-full justify-center items-center gap-3 text-xl">
       <p className="text-center text-white">
-        No wallet found. Please install Metamask to use this app: <a href="https://metamask.io/download.html" target="_blank" rel="noreferrer" className="text-blue-400">https://metamask.io/download.html</a>
+        Wallet not found. Please install Metamask to use this app: <a href="https://metamask.io/download.html" target="_blank" rel="noreferrer" className="text-blue-400">https://metamask.io/download.html</a>
       </p>
     </div>
   );
