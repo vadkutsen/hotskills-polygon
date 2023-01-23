fdd0ac3f281f046c2f1e5670559aa872ee5a5171
vadkutsen <vadkutsen@gmail.com>
2023-01-24 00:43:42 +0100

    Update logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 import { AuthContext } from "../context/AuthContext";
-import logo1 from "../../images/logo1.png";
+import logo1 from "../../images/logo-white.svg";
 import logo2 from "../../images/polygonlogo.png";
 import Wallet from "./Wallet";
 import ConnectWalletButton from "./ConnectWalletButton";
@@ -29,7 +29,7 @@ const Navbar = () => {
       <div className="md:flex-[0.9] flex-initial justify-center items-center">
         <Link to="/">
           <div className="flex gap-2 text-white text-2xl cursor-pointer font-bold">
-            <img alt="Brand logo" className="h-7 self-center" src={logo1} />
+            <img alt="Brand logo" className="h-6 self-center" src={logo1} /><b><span className="text-pink-400">HOT</span>SKILLS</b>
             <img alt="Network logo" className="h-5 self-center" src={logo2} />
           </div>
         </Link>
